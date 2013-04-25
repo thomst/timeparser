889b24d9adccfc029bb1ef33f0b5f0502b6194d4
Thomas Leichtfuss <thomaslfuss@gmx.de>
2013-04-25 16:39:16 +0200

    bugfix: setup.py -> long_description

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from distutils.core import setup
 
 VERSION = "0.1"
@@ -9,8 +10,8 @@ setup(
     author_email = "thomaslfuss@gmx.de",
     url = "https://github.com/thomst/datetimeparser",
     download_url = "https://pypi.python.org/packages/source/d/datetimeparser/datetimeparser-{version}.tar.gz".format(version=VERSION),
-    description = 'little pymodule to parse strings to datetime.time-, -date- or -datetime-objects.',
-    long_description = open('README.rst').read(),
+    description = 'Little python-module to parse strings to datetime.time-, -date- or -datetime-objects.',
+    long_description = open('README.rst').read() if os.path.isfile('README.rst') else str(),
     py_modules = ["datetimeparser"],
     classifiers=[
         'Development Status :: 4 - Beta',
