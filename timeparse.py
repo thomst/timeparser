595c8c927933781e01c6fb7865992c6efb8cb4b9
Thomas Leichtfuss <thomaslfuss@gmx.de>
2013-04-29 17:26:51 +0200

    formats as list-classes
    formats are now child-classes of list. The constructors took a lot of
    arguments to control which formats should be built. If a string is given
    it will be evaluated and the generation of formats will be tailored to
    it.
    
    Another concept of these classes is that they weet out a existing list
    of formats according to a string. But this isn't yet realized straigth.
    Maybe I'll skip that feature...

--- timeparse.py
+++ timeparse.py
@@ -47,54 +47,152 @@ setEndian(guessEndian())
 setToday(datetime.date.today())
 
 
-def getTimeFormats(seps=None):
-    seps = seps or TIME_SEPS
-    formats = ['%X', '%H']
-    no_sec = ['%H', '%M']
-    codes = ['%H', '%M', '%S']
-    for sep in seps:
-        formats.append(sep.join(no_sec))
-        formats.append(sep.join(codes))
-        formats.append(sep.join(codes) + '.%f')
-        formats.append(sep.join(codes) + ' %f')
-    return formats
+class BaseFormats(list):
+    def __init__(self, formats=list(), seps=list(), allow_no_sep=True,
+                    figures=[True, True, True], string=None):
+        super(BaseFormats, self).__init__(formats)
+        self._seps = seps or self.SEPS[:]
+        self._allow_no_sep = allow_no_sep
+        self._figures = figures
+        self._sep = None
+        self._index = 0
+        if string: self._evaluate_string(string)
+        if self and string: self._weed()
+        elif not self: self._generate()
+
+    def _evaluate_string(self, string):
+        try: self._sep = [s for s in self._seps if s in string][0]
+        except IndexError:
+            if self._allow_no_sep: self._seps = list()
+            else: self._figures = [True, False, False]
+        else:
+            figures = string.split(self._sep)
+            if len(figures) == 3: self._figures = [False, False, True]
+            elif len(figures) == 2: self._figures = [False, True, False]
+
+    def _weed(self):
+        #TODO: this doesn't work properly with _evaluate_string...
+        for f in self[:]:
+            if self._sep and not self._sep in f: self.remove(f)
+            if not self._figures[2] and self.FIGURES[2] in f: self.remove(f)
+            if not self._figures[1] and self.FIGURES[1] in f: self.remove(f)
+
+    def _get_code_list(self):
+        pass
+
+    def _generate(self):
+        formats = list()
+        code_list = self._get_code_list()
+        if self._figures[0]: formats.append(self.FIGURES[0])
+        if self._allow_no_sep: self._seps.append(str())
+        for s in self._seps:
+            for codes in code_list: formats.append(s.join(codes))
+        self.extend(formats)
 
 
-def getDateFormats(seps=None, endian=None):
-    seps = seps or DATE_SEPS
-    endian = endian or ENDIAN
-    formats = ['%x', '%d']
-    code_map = {
-        BIG_ENDIAN : ['%y', '%m', '%d'],
-        LITTLE_ENDIAN : ['%d', '%m', '%y'],
-        MIDDLE_ENDIAN : ['%m', '%d', '%y'],
+class TimeFormats(BaseFormats):
+    SEPS = [':', ' ']
+    MICROSEC_SEPS = ['.', ' ']
+    CODES = ['%H', '%M', '%S', '%f']
+    FIGURES = ['%H', '%M', '%S']
+
+    def __init__(self, four_figure=False, *args, **kwargs):
+        self._four_figure = four_figure
+        super(TimeFormats, self).__init__(*args, **kwargs)
+
+    def _get_code_list(self):
+        code_list = list()
+        if self._figures[1]: code_list.append(self.CODES[:2])
+        if self._figures[2]: code_list.append(self.CODES[:3])
+        if self._four_figure:
+            for sep in self.MICROSEC_SEPS:
+                code_list.append(self.CODES[:2] + [sep.join(self.CODES[2:])])
+        return code_list
+
+
+class DateFormats(BaseFormats):
+    SEPS = ['.', '-', '/', ' ']
+    CODES = {
+        'year' : ['%y', '%Y'], 
+        'month' : ['%m', '%b', '%B'],
+        'day' : ['%d']
         }
-    codes = code_map[endian]
-    no_year = codes[:]
-    no_year.remove('%y')
+    FIGURES = ['%d', '%m', '%y']
 
-    #first the no_year-formats, because %m%d should be checked before %y%m%d
-    #otherwise 1124 would be 4.2.2011 instead of 24.11.2013
-    for sep in seps:
-        formats.append(sep.join(no_year))
-        formats.append(sep.join(no_year).replace('m', 'b'))
-        formats.append(sep.join(no_year).replace('m', 'B'))
-        formats.append(sep.join(codes))
-        formats.append(sep.join(codes).replace('y', 'Y'))
-        formats.append(sep.join(codes).replace('m', 'b'))
-        formats.append(sep.join(codes).replace('m', 'B'))
-    return formats
+    def __init__(self, allow_month_name=True, endian=None, *args, **kwargs):
+        self._allow_month_name = allow_month_name
+        self._endian = endian or ENDIAN
+        super(DateFormats, self).__init__(*args, **kwargs)
+
+    @staticmethod
+    def _get_order(endian):
+        if endian == LITTLE_ENDIAN: return ('day', 'month', 'year')
+        if endian == BIG_ENDIAN: return ('year', 'month', 'day')
+        if endian == MIDDLE_ENDIAN: return ('month', 'day', 'year')
+
+    def _get_code_list(self):
+        code_list = list()
+        order = self._get_order(self._endian)
+        code_dict = dict([(k, self.CODES[k][0]) for k in order])
+
+        def get_month_name(order):
+            c_dict = code_dict.copy()
+            c_list = list()
+            for month in self.CODES['month']:
+                c_dict['month'] = month
+                c_list.append([c_dict[k] for k in order])
+            return c_list
+
+        if self._figures[1]:
+            incomplete = list(order)
+            incomplete.remove('year')
+            if self._allow_month_name: code_list.extend(get_month_name(incomplete))
+            else: code_list.append([code_dict[k] for k in incomplete])
+
+        if self._figures[2]:
+            for year in self.CODES['year']:
+                code_dict['year'] = year
+                if self._allow_month_name: code_list.extend(get_month_name(order))
+                else: code_list.append([code_dict[k] for k in order])
+
+        return code_list
 
 
-def getDatetimeFormats(seps=None, date_formats=list(), time_formats=list(), endian=None):
-    seps = seps or DATE_TIME_SEPS
-    endian = endian or ENDIAN
-    df = date_formats or getDateFormats(endian=endian)
-    tf = time_formats or getTimeFormats()
-    datetime_formats = list()
-    for sep in seps:
-        datetime_formats += [sep.join((d, t)) for d in df for t in tf]
-    return datetime_formats
+class DatetimeFormats(BaseFormats):
+    SEPS = [' ', ',', '_', ';']
+    DATE_SEPS = DateFormats.SEPS
+    TIME_SEPS = TimeFormats.SEPS
+
+    def __init__(self, seps=list(), allow_no_sep=True, string=None, date_kwargs=dict(), time_kwargs=dict()):
+        self._seps = seps or self.SEPS
+        self._allow_no_sep = allow_no_sep
+        self._date_kwargs = date_kwargs
+        self._time_kwargs = time_kwargs
+        if string: self._evaluate_string(string)
+        self._generate()
+
+    def _evaluate_string(self, string):
+        #try hard to split the string...
+#        used_seps = re.findall('\W+', string)
+#        common_seps = set(used_seps) & set(self._seps)
+
+        date_seps = self._date_kwargs.get('seps', None) or self.DATE_SEPS
+        time_seps = self._time_kwargs.get('seps', None) or self.TIME_SEPS
+
+        #reduce seps for time- and date-formats:
+        self._seps = [s for s in self._seps if s in string]
+        self._date_kwargs['seps'] = [s for s in date_seps if s in string]
+        self._time_kwargs['seps'] = [s for s in time_seps if s in string]
+
+    def _generate(self):
+        formats = list()
+        date_fmt = DateFormats(**self._date_kwargs)
+        time_fmt = TimeFormats(**self._time_kwargs)
+        if self._allow_no_sep: self._seps.append(str())
+        for s in self._seps:
+            formats += [s.join((d, t)) for d in date_fmt for t in time_fmt]
+        self.extend(formats)
+
 
 
 def parsetime(string, formats=list()):
@@ -112,8 +210,7 @@ def parsetime(string, formats=list()):
     Return a datetime.time-object.
     Raises ValueError if string couldn't be parsed as time.
     """
-    seps = [s for s in TIME_SEPS if s in string][:1]
-    formats = formats or getTimeFormats(seps=seps)
+    formats = formats or TimeFormats(string=string)
     for f in formats:
         try: return datetime.datetime.strptime(string, f).time()
         except ValueError: continue
@@ -135,8 +232,7 @@ def parsedate(string, formats=list(), today=None):
     Return a datetime.date-object.
     Raises ValueError if string couldn't be parsed as date.
     """
-    seps = [s for s in DATE_SEPS if s in string][:1]
-    formats = formats or getDateFormats(seps=seps)
+    formats = formats or DateFormats(string=string)
     today = today or TODAY
     for f in formats:
         try: date = datetime.datetime.strptime(string, f).date()
@@ -168,10 +264,7 @@ def parsedatetime(string, formats=list()):
     Return a datetime.datetime-object.
     Raises ValueError if string couldn't be parsed as datetime.
     """
-    dseps = [s for s in DATE_SEPS if s in string]
-    tseps = [s for s in TIME_SEPS if s in string]
-    dtseps = [s for s in DATE_TIME_SEPS if s in string]
-    formats = formats or getDatetimeFormats(dtseps, getDateFormats(dseps), getTimeFormats(tseps))
+    formats = formats or DatetimeFormats(string=string)
     for f in formats:
         try: return datetime.datetime.strptime(string, f)
         except ValueError: continue
