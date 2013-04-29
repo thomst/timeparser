55f98a4907cb118e68fab4fd2bb63aad5c97459d
Thomas Leichtfuss <thomaslfuss@gmx.de>
2013-04-30 00:34:47 +0200

    v0.2: changed module-name: timeparse became timeparser

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import os
 from distutils.core import setup
 
-VERSION = "0.1"
+VERSION = "0.2"
 
 setup(
-    name = "datetimeparser", 
+    name = "timeparser", 
     version = VERSION, 
     author = "Thomas Leichtfuss", 
     author_email = "thomaslfuss@gmx.de",
-    url = "https://github.com/thomst/datetimeparser",
-    download_url = "https://pypi.python.org/packages/source/d/datetimeparser/datetimeparser-{version}.tar.gz".format(version=VERSION),
-    description = 'Little python-module to parse strings to datetime.time-, -date- or -datetime-objects.',
+    url = "https://github.com/thomst/timeparser",
+    download_url = "https://pypi.python.org/packages/source/d/timeparser/timeparser-{version}.tar.gz".format(version=VERSION),
+    description = 'python-module to parse strings to datetime.time-, -date- or -datetime-objects.',
     long_description = open('README.rst').read() if os.path.isfile('README.rst') else str(),
-    py_modules = ["datetimeparser"],
+    py_modules = ["timeparser"],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -23,5 +23,5 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
     license='GPL',
-    keywords='parser datetime',
+    keywords='parser parse datetime time strings',
 )
